424b1372e5001e6eac2dafe82307b11445995980
domaro <diasomarov90@gmail.com>
2015-04-09 11:10:38 +0100

    stage begining

--- lib/server.rb
+++ lib/server.rb
@@ -1,5 +1,10 @@
 require 'sinatra/base'
-
+require 'data_mapper'
+env = ENV['RACK_ENV'] || 'development'
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+require_relative 'link'
+DataMapper.finalize
+DataMapper.auto_upgrade!
 class BookmarkManager < Sinatra::Base
 
   get '/' do
